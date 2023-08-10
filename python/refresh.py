375227b63cb1ec9a1d6725196531adf33b68e931
InvQ <abdulah.alrifai@outlook.com>
2023-08-10 10:07:20 -0500

    refresh.py updated

--- python/refresh.py
+++ python/refresh.py
@@ -19,3 +19,40 @@
 ##print(((5 + 30) * 20 ) / 10)
 # Without nesting
 ##print(5 + 30 * 20 / 10)
+
+# Variables
+##hundred = 100
+##print(hundred + hundred)
+##print(2==3)
+##f2 = 2==3
+
+##if f2 == False:
+##    print('f2 is false')
+
+##hun = 100
+##hun = 200
+##print(hun)
+
+##john = 100
+##fred = john
+##print(fred)
+
+##num_cn = 200
+##print(num_cn)
+
+##found_coins = 20
+##magic_coins = 10
+##stolen_coins = 2
+
+##print(found_coins + magic_coins * 365 - stolen_coins * 52)
+
+# Strings, Lists, Tuples, and Maps
+
+##str = 'My favorite food is pizza.'
+##str2 = "My favorite food is burgers."
+##str3 = '''How do dinosaurs pay their bills?
+##With tyrannosaurus checks!'''
+##print(str, str2)
+##print(str3)
+
+
