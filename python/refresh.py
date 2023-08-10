9794ae04ab1636855b913963631aa5f2d3f02da5
InvQ <abdulah.alrifai@outlook.com>
2023-08-10 09:48:50 -0500

    refresh.py added to python dir.

---
+++ python/refresh.py
@@ -0,0 +1,21 @@
+# Calculations
+##multiplication = 10 * 10
+##division = 10 / 10
+##addition = 10 + 10
+##subtraction = 10 - 10
+##print(multiplication)
+##print(division)
+##print(addition)
+##print(subtraction)
+
+# ORDER of Operations
+
+##op_1 = 5 + 30 * 20
+##op_2 = (5 + 30) * 20
+
+##print(op_1, op_2)
+
+# Nested paranthesis 
+##print(((5 + 30) * 20 ) / 10)
+# Without nesting
+##print(5 + 30 * 20 / 10)
