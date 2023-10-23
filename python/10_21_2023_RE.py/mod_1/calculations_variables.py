420db2165d20406581d9157f1e7bbf8c97e6f6c3
InvQ <abdulah.alrifai@outlook.com>
2023-10-22 23:52:55 -0600

    wrapping up 'calculations_variables.py' file.

---
+++ python/10_21_2023_RE.py/mod_1/calculations_variables.py
@@ -0,0 +1,44 @@
+# Addition
+print('Addition: ' + str(3 + 3))
+
+# Subtraction
+print('Subtraction: ' + str(3 - 3))
+
+# Division
+print('Division: ' + str(3 / 3))
+
+# Multiplication
+print('Multiplication: ' + str(3 * 3))
+
+
+
+## Order of operations;
+# Follows PEMDAS, parenthesis, exponent, multiplication, 
+# division, addition, subtraction
+# Anything within parantheses is done first.
+
+result_1 = 3 + 4 * 20
+result_2 = (3 + 4) * 20 # Uses parentheses
+
+print(result_1, result_2)
+
+# Variables. Stores info.
+
+hundred = 100
+print(hundred)
+hundred = 200
+print(hundred)
+
+# Variable indepth usage
+print()
+print()
+
+print(20 + 10 * 365 - 3 * 52)
+
+found_coins = 20
+magic_coins = 13
+stolen_coins = 2
+
+print()
+print("Variable printing indepth: ")
+print(found_coins + magic_coins * 365 - stolen_coins * 52)
